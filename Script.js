c96601a19ffc753668c61b421fcc7336c7a04aa9
Jose David Ocampo Piedrahita <jdocampo@cognox.com>
2016-11-10 17:48:16 -0500

    Logo incluido, slides incluidas junto con el script

--- Script.js
+++ Script.js
@@ -1,3 +1,17 @@
 /**
  * Created by jdocampo on 9/11/2016.
  */
+$(function () {
+    var slide = 1;
+    var CanImg = 3;
+    $('.BAfter').on('click',function () {
+        if(slide == 1)
+            slide = (CanImg+1);
+        $('.slides').attr('src','slides/slide'+ --slide +'.png');
+    });
+    $('.BBefore').on('click',function () {
+        if(slide == CanImg)
+            slide=0;
+        $('.slides').attr('src','slides/slide'+ ++slide +'.png');
+    });
+});
