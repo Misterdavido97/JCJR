bc308619ed87c61333578bf5d7a887d9a71ef2e0
Jose David Ocampo Piedrahita <jdocampo@cognox.com>
2016-11-11 14:50:49 -0500

    Corrigindo bugs

--- Script.js
+++ Script.js
@@ -1,9 +1,11 @@
 /**
  * Created by jdocampo on 9/11/2016.
  */
+var slide;
+var CanImg;
 $(function () {
-    var slide = 1;
-    var CanImg = 3;
+    slide = 1;
+    CanImg = 3;
     $('.BAfter').on('click',function () {
         if(slide == 1)
             slide = (CanImg+1);
@@ -22,16 +24,16 @@ $(function () {
         fondo.find('img').height($('body').height());
         fondo.find('img').width($('body').width());
     });
-    var imagen = $('.slide').find('img');
-    var touch = new Hammer(imagen[0]);
-    touch.on('swipeleft',function (event) {
-            if(slide == CanImg)
-                slide=0;
-            $('.slides').attr('src','slides/slide'+ ++slide +'.png');
-    });
-    touch.on('swiperight',function (event) {
-            if(slide == 1)
-                slide = (CanImg+1);
-            $('.slides').attr('src','slides/slide'+ --slide +'.png');
-    });
+});
+var imagen = $('.slide').find('img');
+var touch = new Hammer(imagen[0]);
+touch.on('swipeleft',function (event) {
+    if(slide == CanImg)
+        slide=0;
+    $('.slides').attr('src','slides/slide'+ ++slide +'.png');
+});
+touch.on('swiperight',function (event) {
+    if(slide == 1)
+        slide = (CanImg+1);
+    $('.slides').attr('src','slides/slide'+ --slide +'.png');
 });
